2728e398a6605982ca0807b7455913aaf608fb5d
Stefan <stefan.armijo@gmail.com>
2018-09-17 16:19:36 -0700

    export generate function only

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const randomNestedArray = {};
+// const randomNestedArray = {};
 
 /**
  * Generate an array of length n filled with random numbers between 0 and 99.
  * @param {number} n max length of generated array
  * @returns {array} array of random elements between 0 and 99.
  */
-randomNestedArray.smallArrayGen = function smallArrayGen(max = 6) {
+const smallArrayGen = function smallArrayGen(max = 6) {
   let length = Math.floor(Math.random() * (max)) + 1;
   const result = [];
   while (length > 0) {
@@ -20,15 +20,15 @@ randomNestedArray.smallArrayGen = function smallArrayGen(max = 6) {
  * using smallArrayGen().
  * @param {number} n total number of randomly generated elements in the returned array.
  */
-randomNestedArray.bigArrayGen = function bigArrayGen(n = 30) {
+const bigArrayGen = function bigArrayGen(n = 30) {
   const result = [];
   let totalSize = n;
 
   while (totalSize > 0) {
     if (totalSize > 6) {
-      result.push(this.smallArrayGen(6));
+      result.push(smallArrayGen(6));
     } else {
-      result.push(this.smallArrayGen(totalSize));
+      result.push(smallArrayGen(totalSize));
     }
     totalSize -= result[result.length - 1].length;
   }
@@ -40,8 +40,8 @@ randomNestedArray.bigArrayGen = function bigArrayGen(n = 30) {
  * 'flattened' length. [1, [2, 3, [4]]] has four elements when flattened.
  * @param {number} n Total number of 'flat' elements in returned 2D array.
  */
-randomNestedArray.generate = function generate(n = 30) {
-  const store = this.bigArrayGen(n);
+module.exports = function generate(n = 30) {
+  const store = bigArrayGen(n);
   const result = store.pop();
   let currentArray = result;
 
@@ -56,5 +56,3 @@ randomNestedArray.generate = function generate(n = 30) {
   }
   return result;
 };
-
-module.exports = randomNestedArray;
