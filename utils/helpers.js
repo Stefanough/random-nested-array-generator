7c3a3373c7e5178944b9d1c2f836ce2f361de516
Stefan <stefan.armijo@gmail.com>
2018-09-17 16:18:32 -0700

    utility function to help with testing

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,17 @@
 /**
  * Flattens an arbitrarily deep nested array recursively.
  * @param {array} array Nested array to flatten deeply.
- * @returns {array} Flat array.
+ * @returns {array} new array with no nested arrays.
  */
 function flattenDeep(array) {
+  if (!Array.isArray(array)) {
+    return undefined;
+  }
+
   if (array.length === 0) {
     return [];
   }
+
   let x = array.shift();
   if (Array.isArray(x)) {
     x = flattenDeep(x);
